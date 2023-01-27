0b51d0f15db9c111aa758617d40182c15dd58eea
lee yi xuan <s10243281@connect.np.edu.sg>
2023-01-27 15:54:16 +0800

    update

--- tictactoescript.js
+++ tictactoescript.js
@@ -8,6 +8,22 @@ window.addEventListener('DOMContentLoaded',()=>{
     let currentplayer ='X';
     let isgameactive = true;
 
-    resetbutton.addEventListener('click',resetboard);
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE ='TIE';
+
+    const winningconditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
     
+    resetbutton.addEventListener('click',resetboard);
+
 })
