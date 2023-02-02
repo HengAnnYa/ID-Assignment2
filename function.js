7f020957a6f29abdf0d09f1e04334ad953b07433
HengAnnYa <s10243444@connect.np.edu.sg>
2023-02-02 22:02:11 +0800

    updated main page with images, menu and buttons

--- function.js
+++ function.js
@@ -1,1 +1,6 @@
 const APIKEY = "63d38cac3bc6b255ed0c435d";
+
+const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
+const dropdownList = [...dropdownElementList].map(
+  (dropdownToggleEl) => new bootstrap.Dropdown(dropdownToggleEl)
+);
