ffc021f46cfca88cdfcc61d7165f6927349e88f1
HengAnnYa <s10243444@connect.np.edu.sg>
2023-02-03 18:49:25 +0800

    fixed dropdown, alignment issue found added more styling

--- function.js
+++ function.js
@@ -1,6 +1,35 @@
 const APIKEY = "63d38cac3bc6b255ed0c435d";
 
-const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
-const dropdownList = [...dropdownElementList].map(
-  (dropdownToggleEl) => new bootstrap.Dropdown(dropdownToggleEl)
-);
+function userFunction() {
+  document.getElementById("userDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function settingsFunction() {
+  document.getElementById("settingDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".setting-dropdown")) {
+    var dropdowns = document.getElementsByClassName("setting");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
