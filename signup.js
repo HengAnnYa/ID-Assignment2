b7608c4cf74c0dbe8252987bfb10a3bff5b3b70b
HengAnnYa <s10243444@connect.np.edu.sg>
2023-02-09 14:51:14 +0800

    update api

--- signup.js
+++ signup.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
   //what kind of interface we want at the start
-  const APIKEY = "63d38cac3bc6b255ed0c435d";
-  getSignUp();
+//  const APIKEY = "63d38cac3bc6b255ed0c435d";
+  const APIKEY = "63e4615d478852088da67ef7";
+
+  // getSignUp();
   $("#add-update-msg").hide();
 
   //[STEP 1]: Create our submit form listener
@@ -17,18 +19,20 @@ $(document).ready(function () {
     let ageR = $("#age-range").val();
 
     let jsondata = {
-      name: nameS,
-      username: usernameN,
-      email: emailC,
-      gender: genderN,
-      password: passwordI,
-      age: ageR,
+      "name": nameS,
+      "username": usernameN,
+      "email": emailC,
+      "gender": genderN,
+      "password": passwordI,
+      "age": ageR
     };
+    console.log(jsondata);
 
     let settings = {
       async: true,
       crossDomain: true,
-      url: "https://leaderboard-6a74.restdb.io/rest/user-info",
+//      url: "https://leaderboard-6a74.restdb.io/rest/user-info",
+      url: "https://leaderboard-5aec.restdb.io/rest/user-info",
       method: "POST", //[cher] we will use post to send info
       headers: {
         "content-type": "application/json",
@@ -51,7 +55,7 @@ $(document).ready(function () {
 
       $("#contact-submit").prop("disabled", false);
 
-      getSignUp();
+      // getSignUp();
     });
   });
 });
